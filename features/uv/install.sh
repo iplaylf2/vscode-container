ad38d852bf19f3d0e9b46645381aff0580bd7ed4
i-play-lf2 <914637118@qq.com>
2026-01-06 12:00:58 +0000

    fix

--- features/uv/install.sh
+++ features/uv/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+if [ -z "${BASH_VERSION:-}" ]; then
+  exec bash "$0" "$@"
+fi
+
 set -euo pipefail
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+. $HOME/.local/bin/env
 
 PYTHON_VERSION="${PYTHON_VERSION:-3}"
 if [ "$PYTHON_VERSION" != "none" ]; then
