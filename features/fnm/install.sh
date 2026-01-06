1bf52518be05ced1531c9c9492d824dd0f607876
i-play-lf2 <914637118@qq.com>
2026-01-06 10:27:42 +0000

    fix feature of fnm

--- features/fnm/install.sh
+++ features/fnm/install.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+if [ -z "${BASH_VERSION:-}" ]; then
+  exec bash "$0" "$@"
+fi
+
 set -euo pipefail
 
 curl -fsSL https://fnm.vercel.app/install | bash
+. "$HOME/.bashrc"
 
 NODE_VERSION="${NODE_VERSION:-lts}"
 if [ "$NODE_VERSION" != "none" ]; then
-  fnm install "$NODE_VERSION"
+  if [ "$NODE_VERSION" = "lts" ]; then
+    fnm install --lts
+  else
+    fnm install "$NODE_VERSION"
+  fi
 fi
